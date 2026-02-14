e605b597fc16da67df7ec068d4ca72a6a7601909
Master! <404mikey2013@gmail.com>
2026-02-14 19:39:12 +0100

    Create gmMapping.js

---
+++ gmMapping.js
@@ -0,0 +1,10 @@
+export const GM_TO_NOTEBLOCK = {
+  1: { instrument: "harp", octaveShift: 0 },
+  2: { instrument: "pling", octaveShift: 0 },
+  25: { instrument: "guitar", octaveShift: 1 },
+  33: { instrument: "bass", octaveShift: 2 },
+  74: { instrument: "flute", octaveShift: -1 },
+  81: { instrument: "bit", octaveShift: 0 },
+  106: { instrument: "banjo", octaveShift: 0 },
+  47: { instrument: "harp", octaveShift: 0 }
+}
