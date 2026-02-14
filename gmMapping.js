89d42fc22f515572b714d1f87efdfb7dd5397f62
Master! <404mikey2013@gmail.com>
2026-02-14 19:41:18 +0100

    Update gmMapping.js

--- gmMapping.js
+++ gmMapping.js
@@ -1,10 +1,16 @@
 export const GM_TO_NOTEBLOCK = {
-  1: { instrument: "harp", octaveShift: 0 },
-  2: { instrument: "pling", octaveShift: 0 },
-  25: { instrument: "guitar", octaveShift: 1 },
-  33: { instrument: "bass", octaveShift: 2 },
-  74: { instrument: "flute", octaveShift: -1 },
-  81: { instrument: "bit", octaveShift: 0 },
-  106: { instrument: "banjo", octaveShift: 0 },
-  47: { instrument: "harp", octaveShift: 0 }
+  0: { instrument: "harp", octaveShift: 0 },
+  1: { instrument: "bass", octaveShift: 1 },
+  2: { instrument: "snare", octaveShift: 0 },
+  3: { instrument: "hat", octaveShift: 0 },
+  4: { instrument: "bass", octaveShift: 0 },
+  5: { instrument: "pling", octaveShift: 0 },
+  6: { instrument: "flute", octaveShift: 0 },
+  7: { instrument: "bell", octaveShift: 0 },
+  8: { instrument: "guitar", octaveShift: 0 },
+  9: { instrument: "bass", octaveShift: 2 },
+  10:{ instrument: "bit", octaveShift: 0 },
+  11:{ instrument: "banjo", octaveShift: 0 },
+  12:{ instrument: "pling", octaveShift: 1 },
+  // … añadir todos los 128 programas GM
 }
