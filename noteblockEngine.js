e1ae914813f79e445a2a236122f23266c45f5912
Master! <404mikey2013@gmail.com>
2026-02-14 19:41:56 +0100

    Update noteblockEngine.js

--- noteblockEngine.js
+++ noteblockEngine.js
@@ -1,9 +1,9 @@
 import { GM_TO_NOTEBLOCK } from "./gmMapping.js"
 
-const BASE_MIDI = 54 // F#3
+const BASE_MIDI = 54 // F#3 base
 
 function secondsToTicks(seconds) {
-  return Math.round(seconds * 20)
+  return Math.round(seconds * 20) // 20 ticks por segundo
 }
 
 function midiToNote(midi, octaveShift) {
@@ -16,22 +16,20 @@ export function convertToNoteblocks(midiData) {
 
   midiData.events.forEach(event => {
 
-    const program = event.program || 1
-    const mapping = GM_TO_NOTEBLOCK[program] || { instrument: "harp", octaveShift: 0 }
+    const mapping = GM_TO_NOTEBLOCK[event.program] || { instrument: "harp", octaveShift: 0 }
 
     const tick = secondsToTicks(event.time)
     const note = midiToNote(event.midi, mapping.octaveShift)
 
-    if (note < 0 || note > 24) return
+    if (note < 0 || note > 24) return // Minecraft limit 25 notes
 
     blocks.push({
-      x: tick,
-      y: event.track,
-      z: 0,
+      x: tick,      // eje X = tiempo
+      y: note,      // eje Y = altura de la nota
+      z: event.track, // eje Z = track
       instrument: mapping.instrument,
       note
     })
-
   })
 
   return blocks
