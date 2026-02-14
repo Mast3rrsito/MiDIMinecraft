36c5cf32c4a46c06ca7a5fc805a23ffa58a63c80
Master! <404mikey2013@gmail.com>
2026-02-14 19:42:44 +0100

    Update litematicWriter.js

--- litematicWriter.js
+++ litematicWriter.js
@@ -1,41 +1,51 @@
-import { writeUncompressed } from "./nbt.js"
+import { NBTWriter } from "./nbt.js"
 import { gzip } from "./gzip.js"
 
 export function generateLitematic(blocks) {
+  const writer = new NBTWriter()
 
-  const palette = {}
-  const paletteArray = []
+  writer.writeTag_Compound("", () => {
+    writer.writeTag_String("Author", "MIDI2MC")
+    writer.writeTag_String("Name", "MIDI Song")
+    writer.writeTag_Int("Version", 3)
 
-  blocks.forEach(b => {
-    const key = `minecraft:note_block[instrument=${b.instrument},note=${b.note},powered=false]`
-    if (!palette[key]) {
-      palette[key] = paletteArray.length
-      paletteArray.push({
-        Name: "minecraft:note_block",
-        Properties: {
-          instrument: b.instrument,
-          note: String(b.note),
-          powered: "false"
-        }
-      })
-    }
+    // Palette
+    const paletteMap = {}
+    const paletteArray = []
+    blocks.forEach(b => {
+      const key = `${b.instrument}_${b.note}`
+      if (!paletteMap[key]) {
+        paletteMap[key] = paletteArray.length
+        paletteArray.push({
+          Name: "minecraft:note_block",
+          Properties: {
+            instrument: b.instrument,
+            note: String(b.note),
+            powered: "false"
+          }
+        })
+      }
+    })
+
+    // escribir lista de palette
+    writer.writeTag_List("BlockStatePalette", 10, paletteArray.map(p => {
+      return { properties: p.Properties, name: p.Name }
+    }))
+
+    // BlockStates simplificado: index en palette
+    const stateIndices = blocks.map(b => paletteMap[`${b.instrument}_${b.note}`])
+    writer.writeTag_List("BlockStates", 3, stateIndices)
+
+    // Posiciones
+    writer.writeTag_List("Blocks", 10, blocks.map(b => {
+      const w = new NBTWriter()
+      w.writeTag_Int("x", b.x)
+      w.writeTag_Int("y", b.y)
+      w.writeTag_Int("z", b.z)
+      w.writeTag_Int("state", paletteMap[`${b.instrument}_${b.note}`])
+      return w
+    }))
   })
 
-  const nbtData = {
-    Metadata: {
-      Name: "MIDI Song",
-      Author: "MIDI2MC",
-      RegionCount: 1
-    },
-    Regions: {
-      Song: {
-        Size: { x: 5000, y: 50, z: 1 },
-        BlockStatePalette: paletteArray,
-        BlockStates: new Array(blocks.length).fill(0)
-      }
-    }
-  }
-
-  const raw = writeUncompressed(nbtData)
-  return gzip(raw)
+  return gzip(writer.getArray())
 }
